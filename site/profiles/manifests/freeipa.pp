abd26e713cb9cb89090658bda8c4f40e52cf7d79
GlitchWraith <glitchwraith@ghostlink.net>
2020-10-26 14:47:19 +0000

    FreeIPA Client

--- site/profiles/manifests/freeipa.pp
+++ site/profiles/manifests/freeipa.pp
@@ -27,8 +27,8 @@ class profiles::freeipa (
       password_usedto_joindomain  => $principal_auth,
       puppet_admin_password       => 'NOT Needed For THIS',
       directory_services_password => 'Still NOT Needed',
-      ipa_server_package_name     => 'idm:DL1/server',
-      ipa_client_package_name     => 'idm:DL1/client'
+      ipa_server_package_name     => '@idm:DL1/server',
+      ipa_client_package_name     => '@idm:DL1/client'
     }
   }
   if  $kind == 'master'  {
@@ -44,10 +44,22 @@ class profiles::freeipa (
 
   if  $kind == 'client'  {
 
-    notify{ 'Installs packages only,': }
-
-    package{$freeipa::ipa_client_package_name:
-      ensure => present,
+    class {'freeipa':
+      ipa_role                    => $kind,
+      domain                      => lookup('ipaRealmName').downcase,
+      realm                       => lookup('ipaRealmName'),
+      install_epel                => false, # managed elsewhere
+      ipa_master_fqdn             => lookup('ipaRealmMaster'),
+      # Could use profile::base::cacert::ldap_server but that might create some loops
+      install_ipa_server          => false,
+      ip_address                  => $::ipaddress6,
+      ipa_server_fqdn             => $::fqdn,
+      principal_usedto_joindomain => $join_principal,
+      password_usedto_joindomain  => $principal_auth,
+      puppet_admin_password       => 'NOT Needed For THIS',
+      directory_services_password => 'Still NOT Needed',
+      ipa_server_package_name     => '@idm:DL1/server',
+      ipa_client_package_name     => '@idm:DL1/client'
     }
   }
 
